a6e35deac209b67d00b360f710fc6987951f197c
harp515 <harp5151017@gmail.com>
2024-07-17 12:35:16 +0900

    Feat / HpBar :: 이해도 바 구현, 바 이미지 이동

---
+++ Assets/Scripts/Bar.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bar : MonoBehaviour
+{
+    [SerializeField] private Slider _hpSlider;
+    public GameObject image;
+    
+    [SerializeField]private float maxHp;
+    private float _hp;
+
+    private void Awake()
+    {
+        _hp = maxHp / 2;
+    }
+
+    private void Update()
+    {
+        _hpSlider.value = _hp / maxHp;
+        
+        var thisPos = image.transform.localPosition;
+        image.transform.localPosition = new Vector3((_hpSlider.value * 1000) - 500, thisPos.y,thisPos.z);
+    }
+
+    private void Calculate(float count)
+    {
+        
+    }
+
+}
